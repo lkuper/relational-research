53e02c2e8579e586f19ac2934958285e5353ebc2
lindseykuper <lindseykuper@gmail.com>
2009-06-11 02:34:57 +0000

    Added a new test for commutative conjunction; (hopefully) prettier indentation.

--- summer-research-2009/babymk/babymktests.scm
+++ summer-research-2009/babymk/babymktests.scm
@@ -32,23 +32,23 @@
 (test-check "test0"
   (run 5 (q)
     (and2
-      (== q 1)
+     (== q 1)
+     (and2
+      (print-s)
       (and2
-        (print-s)
-        (and2
-          (== 2 2)
-          (print-s)))))
+       (== 2 2)
+       (print-s)))))
   `(1))
 
 (test-check "test1"
   (run 5 (q)
     (and2
-      (print-s)
-      (and2
-        (or2
-          (== q 1)
-          (== q 2))
-        (print-s))))
+     (print-s)
+     (and2
+      (or2
+       (== q 1)
+       (== q 2))
+      (print-s))))
   `(1 2))
 
 (test-check "test2"
@@ -59,47 +59,47 @@
 (test-check "test3"
   (run 2 (q)
     (or2
-      (== 1 q)
-      (or2
-        (== 2 q)
-        (lambdag@ (s)
-          ((lambda (x) (x x))
-           (lambda (x) (x x)))))))
+     (== 1 q)
+     (or2
+      (== 2 q)
+      (lambdag@ (s)
+        ((lambda (x) (x x))
+         (lambda (x) (x x)))))))
   `(1 2))
 
 (test-check "test4"
   (run #f (q)
     (and2
-      (== #f q)
-      (project q (lambda (q) (== (not (not q)) q)))))
+     (== #f q)
+     (project q (lambda (q) (== (not (not q)) q)))))
   '(#f))
 
 (test-check "test5"
   (run #f (q)
     (and2
-      (== #f q)
-      (project q (lambda (y) (== (not (not y)) y)))))
+     (== #f q)
+     (project q (lambda (y) (== (not (not y)) y)))))
   '(#f))
 
 (test-check "test6"
   (run #f (q)
     (and2
-      (== #f q)
-      (project q (lambda (q) (== (not (not q)) q)))))
+     (== #f q)
+     (project q (lambda (q) (== (not (not q)) q)))))
   '(#f))
 
 (test-check "test7"
   (run #f (q)
     (existfn
-        (lambda (z)
-          (existfn
-            (lambda (y)
-              (and2
-                (== #f y)
-                (project z (lambda (z)
-                             (and2
-                               (== (not (not z)) z)
-                               (== `(,y ,z) q))))))))))
+     (lambda (z)
+       (existfn
+        (lambda (y)
+          (and2
+           (== #f y)
+           (project z (lambda (z)
+                        (and2
+                         (== (not (not z)) z)
+                         (== `(,y ,z) q))))))))))
   '((#f #t)))
 
 (define anyo
@@ -113,8 +113,16 @@
 (test-check "test9"
   (run 20 (q)
     (and2
-      (print-s)
-      (or2
-       (anyo (== #f #f))
-       (and2 (print-s) (anyo (== #t q))))))
+     (print-s)
+     (or2
+      (anyo (== #f #f))
+      (and2 (print-s) (anyo (== #t q))))))
   '(_.0 #t _.0 #t _.0 #t _.0 #t _.0 #t _.0 #t _.0 #t _.0 #t _.0 #t _.0 #t))
+
+;; This one won't terminate until we get commutative conjunction
+;; working.
+(define cc-test
+  (lambda ()
+    (and2 (anyo (== #f #f)) (== #f #t))))
+
+(run 2 (q) (cc-test))
