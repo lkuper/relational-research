1ad3cfd16898ec6104b25e352b486bc5bf69e1f3
davecbender <davecbender@gmail.com>
2009-08-17 22:39:59 +0000

    deal correctly with files that have multiple top-level forms

--- subst/scrub-code.scm
+++ subst/scrub-code.scm
@@ -2,6 +2,19 @@
 ;        another directory. Is there a better way?
 (load "~/repos/iucs-relational-research/subst/lib/pmatch.scm")
 
+(define functions '((walk-basic . walk)
+                    (unify-sv . unify)
+                    (step . step-sref)
+                    (walk-no-rec-stkf . walk-flatten)
+                    (walk-no-rec-stkf-back . walk-flatten-back)
+                    (walk-fb . walk-front-back)
+                    (walk-no-rec-stk . walk-reverse-list)
+                    (walk-no-rec-stk-back . walk-reverse-list-back)
+                    (walk-pinch-s . walk-pinch)
+                    (pinch-s . pinch)
+                    (pinch-s-find . pinch-find)
+                    (ret-no-rec-stkf . ret-flatten)))
+
 ;; scrub-code: strips statistics-gathering code, extraneous begins,
 ;; and lhs and rhs out of source code.  Only tested on walk-basic, so
 ;; far.
@@ -13,14 +26,27 @@
       [((inc-ws-calls ,x) . ,cdr) (scrub-code cdr)]
       [((inc-ws-recrs) . ,cdr) (scrub-code cdr)]
       [((ws-found-match) . ,cdr) (scrub-code cdr)]
+      ;; function definition rename rules
+      [(define . ,d)
+       (cond
+         ((assq (car d) functions) => (lambda (p)
+                                        `(define ,(cdr p) . ,(scrub-code (cdr d)))))
+         (else `(define . ,(scrub-code d))))]
       [(lhs . ((car ,e))) `(caar ,(scrub-code e))]
       [(rhs . ((car ,e))) `(cdar ,(scrub-code e))]
       ;; this one's gross, but I don't have a better way at the moment
       [(begin . ,e) (car (scrub-code e))]
+      ;; function application rename rules
+      [(,f . ,d) (guard (assq f functions))
+       `(,(cdr (assq f functions)) . ,(scrub-code d))]
       [,otherwise (cons (scrub-code (car expr))
                         (scrub-code (cdr expr)))])))
 
 (let* ([input-file (cadr (command-line))]
-       [p (open-input-file input-file)]
-       [code (read p)])
-  (pretty-print (scrub-code code)))
+       [p (open-input-file input-file)])
+  (let loop ((code (read p)))
+    (cond
+      ((eof-object? code) (void))
+      (else (begin
+              (pretty-print (scrub-code code))
+              (loop (read p)))))))
